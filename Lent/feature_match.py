Debugged attention jacobian, but not entirely sure if returns correct vals

Lent/feature_match.py
@@ -2,27 +2,23 @@
 import torch
 from basic1_models import *
 
-def feature_map_diff(student_output, teacher_output, aggregate_chan):
+def feature_map_diff(s_map, t_map, aggregate_chan):
     """Compute the difference between the feature maps of the student and teacher models.
-    
     Args:
-        student_output: torch tensor, activation map of teacher model [batch_size, num_channels]
-        teacher_output: torch tensor, output of teacher model [batch_size, num_channels]
+        s_map: torch tensor, activation map of teacher model [batch_size, num_channels]
+        t_map: torch tensor, output of teacher model [batch_size, num_channels]
         aggregate_chan: bool, whether to aggregate the channels of the feature activation
     """
     # Aggregate the channels of the feature activation using root squared absolute value of channels to create activation map
     if aggregate_chan:
-        student_output = torch.sqrt(torch.sum(torch.abs(student_output)**2, dim=1))
-        teacher_output = torch.sqrt(torch.sum(torch.abs(teacher_output)**2, dim=1))
-
+        s_map = torch.sqrt(torch.sum(torch.abs(s_map)**2, dim=1))
+        t_map = torch.sqrt(torch.sum(torch.abs(t_map)**2, dim=1))
     # Compute the difference between the feature maps
-    diff = torch.norm( (student_output/torch.norm(student_output, p=2) - teacher_output/torch.norm(teacher_output, p=2) ), p=2)
-    
+    diff = torch.mean(s_map/torch.norm(s_map, p=2, dim=-1).unsqueeze(-1) - t_map/torch.norm(t_map, p=2, dim=-1).unsqueeze(-1) )
     return diff
 
 def feature_extractor(model, inputs, batch_size, layer_num):
     """Extract feature maps from model.
-    
     Args:
         model: torch model, model to extract feature maps from
         inputs: torch tensor, input to model
@@ -41,11 +37,5 @@ def feature_extractor(model, inputs, batch_size, layer_num):
     layer = list(model.children())[0][layer_num] # Length 2 list so get 0th index to access layers
     layer.register_forward_hook(get_activations(batch_size))  # Register hook
     model(inputs) # Forward pass
-    # map = features[0].view(batch_size, -1)
-    # i = torch.argmax(map, dim=1)
-    # model.zero_grad()
-    # gradient = torch.ones_like(map[:,i])
-    # map[:,i].backward(gradient)
-    # grads = inputs.grad
     return features[0]
     

Lent/image_distillation.py
@@ -10,6 +10,7 @@ from plotting import *
 from jacobian_srinivas import *
 from contrastive import *
 from feature_match import *
+from utils_ekdeep import *
 
 # Setup ========================================================================
 # Suppress warnings "divide by zero" produced by NaN gradients
@@ -86,12 +87,8 @@ def fine_tune(model, dataloader):
             inputs = inputs.to(device)
             labels = labels.to(device)
             scores = model(inputs)
-            # scores.requires_grad = True
-            # scores =  nn.functional.softmax(model(input
-            # s), dim=1)
-            # _, preds = torch.max(scores, 1)
             optimizer.zero_grad()
-            loss = F.cross_entropy(scores, labels)
+            loss = ce_loss(scores, labels)
             loss.backward()
             optimizer.step()
             if it % 100 == 0:
@@ -122,20 +119,20 @@ def train_distill(loss, teacher, student, lr, epochs, repeats, title, **kwargs):
             for inputs, labels in tqdm(train_loader):
                 inputs = inputs.to(device)
                 inputs.requires_grad = True
-                inputs.retain_grad = True
                 labels = labels.to(device)
                 scores = student(inputs)
                 targets = teacher(inputs)
                 input_dim = 32*32*3
                 output_dim = scores.shape[1]
                 
-                # loss = jacobian_loss(scores, targets, inputs, 1, 0.8, batch_size, kl_loss, input_dim, output_dim)
-                
                 s_map = feature_extractor(student, inputs, batch_size, 2)
                 t_map = feature_extractor(teacher, inputs, batch_size, 2)
                 
                 # loss = jacobian_attention_loss(scores, targets, s_map, t_map, batch_size, T=1, alpha=0.5, loss_fn=kl_loss)
-                loss = jacobian_loss(s_map, t_map, inputs, 1, 0.8, batch_size, kl_loss, input_dim, s_map.shape[-1])
+                # loss = jacobian_loss(s_map, t_map, inputs, 1, 0.8, batch_size, kl_loss, input_dim, s_map.shape[-1])
+                # loss = jacobian_loss(scores, targets, inputs, 1, 0.8, batch_size, kl_loss, input_dim, output_dim)
+                # loss = feature_map_diff(s_map, t_map, False)
+                loss = jacobian_attention_loss(student, teacher, scores, targets, inputs, batch_size, 1, 0.8, kl_loss)
                 
                 loss.backward()
                 optimizer.zero_grad()

Lent/jacobian_srinivas.py
@@ -30,7 +30,6 @@ def jacobian_loss(scores, targets, inputs, T, alpha, batch_size, loss_fn, input_
     loss = (1-alpha) * distill_loss + alpha * jacobian_loss
     return  loss
 
-# DEBUGGED
 def get_approx_jacobian(output, x, batch_size, input_dim, output_dim):
     """Rather than computing Jacobian for all output classes, compute for most probable class.
     Args:
@@ -56,7 +55,7 @@ def get_jacobian(output, x, batch_size, input_dim, output_dim):
 
 #===================================================================================================
 
-def jacobian_attention_loss(scores, targets, s_map, t_map, batch_size, T, alpha, loss_fn):
+def jacobian_attention_loss(student, teacher, scores, targets, inputs, batch_size, T, alpha, loss_fn):
     """Eq 10, attention map Jacobian vector.
     J = dZ/dX where X is all inputs and Z is channel-wise square of attention maps.
     Args:
@@ -66,64 +65,49 @@ def jacobian_attention_loss(scores, targets, s_map, t_map, batch_size, T, alpha,
         alpha: float, weight of the jacobian penalty
         loss_fn: base loss function - MSE, CE, KL
     """
-    s_jac = get_activation_jacobian(scores, s_map, batch_size)
-    t_jac = get_activation_jacobian(targets, t_map, batch_size)
-    dims = [d for d in range(1, s_jac.dims+1)]    # Don't add batch dimension to norm calculation
+    s_jac = get_grads(student, inputs, batch_size, 3)
+    t_jac = get_grads(teacher, inputs, batch_size, 3)
     soft_pred = F.softmax(scores/T, dim=1)
     soft_targets = F.softmax(targets/T, dim=1)
-    
-    s_norm = s_jac / torch.norm(s_jac, 2, dim=dims).unsqueeze(1) 
-    t_norm = t_jac / torch.norm(t_jac, 2, dim=dims).unsqueeze(1)
-    jacobian_loss = torch.norm(s_norm - t_norm, 2)**2
+
+    s_norm = s_jac / torch.norm(s_jac, 2, dim=-1).unsqueeze(1) 
+    t_norm = t_jac / torch.norm(t_jac, 2, dim=-1).unsqueeze(1)
+    jacobian_loss = torch.norm(s_norm - t_norm, 2)
     jacobian_loss = torch.mean(jacobian_loss)   # Batchwise mean
     distill_loss = T**2 * loss_fn(soft_pred, soft_targets)
     loss = (1 - alpha) * distill_loss + alpha * jacobian_loss
     return loss
 
-# Deprecated function until I figure out how to fix a bug
-def get_activation_jacobian(x, feature_map, batch_size):
-    """Rather than computing Jacobian for entire activation map, compute for largest activation.
-    Args:
-        output: [batch_size, output_dim=channels*width*height]
-        x: input with requres_grad() True - [batch_size, input_dim, input_dim, channels]
-    """
-    input_dim = x.view(batch_size, -1).shape[-1]
-    output_dim = feature_map.shape[-1]
-     # Largest pixel in feature map
-    i = torch.argmax(feature_map, dim=1)
-    grad_output = torch.zeros(batch_size, output_dim, device=x.device)
-    grad_output[:, i] = 1
-    feature_map.retain_grad()
-    x.requires_grad = True
-    jacobian = torch.autograd.grad(feature_map, x, grad_output, retain_graph=True, allow_unused=True)[0]
-    return jacobian.view(batch_size, input_dim)
-
 def get_grads(model, inputs, batch_size, layer_num):
-    """Extract feature maps from model.
-    
+    """Extract feature maps from model and call backward() to get input grad.
     Args:
         model: torch model, model to extract feature maps from
         inputs: torch tensor, input to model
         layer_num: int, layer number to extract feature maps from
     """
-    def get_activations(batch_size):
+    inputs.requires_grad = True
+    def get_activations():
         def forward_hook(model, input, output):
-            output = output.view(batch_size, -1)
-            output = torch.autograd.Variable(output.data, requires_grad=True)
-            output.retain_grad()
             features.append(output)
         return forward_hook
+
     features = []
     layer = list(model.children())[0][layer_num] # Length 2 list so get 0th index to access layers
-    layer.register_forward_hook(get_activations(batch_size))  # Register hook
+    layer.register_forward_hook(get_activations())  # Register hook
     model(inputs) # Forward pass
     
     map = features[0].view(batch_size, -1)
     i = torch.argmax(map, dim=1)
-    model.zero_grad()
     gradient = torch.ones_like(map[:,i])
-    map[:,i].backward(gradient)
-    grads = inputs.grad
+    gradient.requires_grad = True
+    map[:,i].backward(gradient, retain_graph=True)
+    grads = inputs.grad.view(batch_size, -1)
+
+    # Clear grads and set requires_grad to True
+    inputs.grad.detach().zero_()
+    model.zero_grad()
+    grads.requires_grad = True
+
     return grads
     
 # TODO: implement