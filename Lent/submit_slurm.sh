53706609f23c2bf1c44cee331c02ae32ee8a9367
CindyXWu <wu.cindyx@gmail.com>
2023-03-19 02:14:16 +0000

    Debugged config files

--- Lent/submit_slurm.sh
+++ Lent/submit_slurm.sh
@@ -2,13 +2,12 @@
 
 # Set the argument to pass to myscript.sh
 config_file="LeNet_exp_loss_configs.yml"
-config_data=$(python -c "import yaml; print(yaml.safe_load(open('$config_file')))")
-configs=($(python -c "import yaml; print(' '.join(yaml.safe_dump($config_data).splitlines()))"))
+configs=$(python -c "import yaml; print(yaml.safe_load(open('$config_file')))")
 
 # Define the number of configurations
-num_configs=${#configs[@]}
+num_configs=$(python -c "import yaml; print(len(yaml.safe_load(open('$config_file'))))")
 
-# Loop through each configuration and run the Python script
+ Loop through each configuration and run the Python script
 for ((i=0; i<$num_configs; i++))
 do
     sbatch --export=CONFIG_FILE=$config_file,IDX=$i batch_student
