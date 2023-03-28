fa7ab848331d77ec1ec76da1a79201e8e49d67b5
CindyXWu <wu.cindyx@gmail.com>
2023-03-28 15:32:43 +0100

    Debugged array job submission

--- Lent/submit_array.sh
+++ Lent/submit_array.sh
@@ -2,3 +2,5 @@
 
 config_file="CIFAR100_distill.yml"
 num_configs=$(python -c "import yaml; print(len(yaml.safe_load(open('$config_file'))))")
+
+sbatch --export=config_file=$config_file --array=0-$((num_configs-1)) array_s
---
+++ Lent/submit_tslurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set the argument to pass to myscript.sh
+config_file="shapes_t.yml"
+configs=$(python -c "import yaml; print(yaml.safe_load(open('$config_file')))")
+
+# Define the number of configurations
+num_configs=$(python -c "import yaml; print(len(yaml.safe_load(open('$config_file'))))")
+
+# Loop through each configuration and run the Python script
+for ((i=0; i<$num_configs; i++))
+do
+    sbatch --export=CONFIG_FILE=$config_file,IDX=$i batch_t_shapes
+done
