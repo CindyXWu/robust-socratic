fe73ee7cd19f82ab939223638ab8ddad76620c64
CindyXWu <wu.cindyx@gmail.com>
2023-04-11 18:42:56 +0100

    Scripting

--- Lent/yaml_s.py
+++ Lent/yaml_s.py
@@ -6,10 +6,10 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # Define the filename for your configuration file
 dataset = "Dominoes"
 filename = dataset+"_s.yml"
-s_exp_nums = [0, 1]
-t_exp_nums = [0, 1, 2]
-teacher_nums = [3]
-student_nums = [2]
+s_exp_nums = [0, 1, 2, 3, 4, 5, 6]
+t_exp_nums = [0, 1, 2, 3, 4, 5, 6]
+teacher_nums = [1]
+student_nums = [1]
 loss_nums = [0, 1, 2]
 aug_nums = [0]
 
