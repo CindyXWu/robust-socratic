174331ee9dc9afe50547235f899f0468bc45fc76
CindyXWu <wu.cindyx@gmail.com>
2023-05-14 02:54:47 +0100

    Got teachers for new Dominoes

--- Lent/slurm/submit_array.sh
+++ Lent/slurm/submit_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-config_file="configs/Shapes_t.yml"
+config_file="configs/Dominoes_s.yml"
 rel_config_file="../${config_file}"
 num_configs=$(python -c "import yaml; print(len(yaml.safe_load(open('$rel_config_file'))))")
 
-sbatch --export=config_file=$config_file --array=0-$((num_configs-1)) array_t
+sbatch --export=config_file=$config_file --array=0-$((num_configs-1)) array_s
