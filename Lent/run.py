0690188dcf57158661a87a17d554a83b609c6a3e
Cindy Wu <wu.cindyx@gmail.com>
2023-08-13 01:44:11 +0100

    Tiny debug on loading sweep config file path

--- Lent/run.py
+++ Lent/run.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     config: dict = load_config(f"configs/{config_filename}.yaml")
     if config.get("is_sweep"):
         wandb_project_name = f"{config['wandb_project_name']} DISTILL {config['model_type']} {config['dataset_type']} {config['config_type']} {config['dataset']['box_cue_pattern']}"
-        sweep_config = load_config(f"configs/{sweep_filename}.yaml")
+        sweep_config = load_config(f"configs/sweep/{sweep_filename}.yaml")
         sweep_params = list(sweep_config['parameters'].keys())
         sweep_id = wandb.sweep(
             sweep=sweep_config,
