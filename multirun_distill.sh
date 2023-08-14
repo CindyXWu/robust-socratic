7fbbdd4a4dac60d51df1de660e6a386212570101
CindyXWu <wu.cindyx@gmail.com>
2023-08-14 18:05:44 +0000

    Merged with main

--- multirun_distill.sh
+++ multirun_distill.sh
@@ -2,11 +2,11 @@
 
 # Path to your script
 SCRIPT_PATH="Lent/run_distill.py"
-experiment="exhaustive_1"
+experiment="exhaustive_0"
 experiment_s="exhaustive_0,exhaustive_1,exhaustive_2,exhaustive_3,exhaustive_4,exhaustive_5,exhaustive_6"
-args=""
+args="distill_loss_type=BASE, dataset.box_cue_type=MANDELBROT"
 
 for i in {1..5}
 do
-    python $SCRIPT_PATH -m +experiment=$experiment +experiment_s=$experiment_s
+    python $SCRIPT_PATH -m +experiment=$experiment +experiment_s=$experiment_s $args
 done
