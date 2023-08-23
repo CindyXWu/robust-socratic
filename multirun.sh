e1d31fa457a4728ab5b893db4d2deb79515389f9
CindyXWu <wu.cindyx@gmail.com>
2023-08-23 22:11:41 +0000

    Merging with RTX 4090

--- multirun.sh
+++ multirun.sh
@@ -4,11 +4,11 @@
 # It does not manage the sweep file name as of 14/08
 SCRIPT_PATH="Lent/run_distill.py"
 config_filename="distill_config"
-experiment="exhaustive_3"
-experiment_s="exhaustive_0,exhaustive_1,exhaustive_2,exhaustive_3,exhaustive_4,exhaustive_5,exhaustive_6"
-args="distill_loss_type=JACOBIAN dataset.box_cue_pattern=RANDOM"
+experiment="targeted_0"
+experiment_s="targeted_0,targeted_3,targeted_4"
+args="distill_loss_type=JACOBIAN"
 
-for i in {1..5}
+for i in {1..3}
 do
     python $SCRIPT_PATH -m +experiment=$experiment +experiment_s=$experiment_s $args --config-name=$config_filename
 done
